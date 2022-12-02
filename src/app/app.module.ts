dfde4d18189747c22ad77b20c1ed73aa50031f30
Audrey Gentili <audrey.gentili38210@gmail.com>
2022-12-02 16:44:10 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { FormsComponent } from './forms/forms.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { MapComponent } from './map/map.component';
+import { IgxBottomNavModule, IgxIconModule } from "igniteui-angular";
 
 @NgModule({
   declarations: [
@@ -18,9 +20,14 @@ import { MapComponent } from './map/map.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    IgxBottomNavModule,
+    IgxIconModule,
     AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
+
 export class AppModule { }
