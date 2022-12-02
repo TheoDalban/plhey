5c0002182e3a983bb7089ae36a0e98abaa7b05d2
Audrey Gentili <audrey.gentili38210@gmail.com>
2022-12-02 17:39:13 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { FormsComponent } from './forms/forms.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { MapComponent } from './map/map.component';
-import { IgxBottomNavModule, IgxIconModule } from "igniteui-angular";
 import { JeuComponent } from './jeu/jeu.component';
 
 @NgModule({
@@ -22,10 +19,6 @@ import { JeuComponent } from './jeu/jeu.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    IgxBottomNavModule,
-    IgxIconModule,
     AppRoutingModule,
   ],
   providers: [],
