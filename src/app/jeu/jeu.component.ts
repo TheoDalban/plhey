311f3e02340c2fbd4cf4f403a599e809ef39c603
TheoDalban <60937138+TheoDalban@users.noreply.github.com>
2022-12-08 11:55:12 +0100

    jeu avancement

--- src/app/jeu/jeu.component.ts
+++ src/app/jeu/jeu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MarkerService } from '../services/marker.service';
 
 @Component({
   selector: 'app-jeu',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class JeuComponent {
   // https://boardgamegeek.com/xmlapi2/id?3
+
+  tabJeu = [XMLDocument];
+
+  axios = require('axios');
+  params = {
+    access_key: '56a31143cc4ed614dbc4820933d1df34',
+    query: 'hot?type=boardgame'
+  };
+
+  coord = [];
+
+  constructor(private markerService: MarkerService) {}
+
+  ngOnInit(): void {
+    this.axios.get('https://boardgamegeek.com/xmlapi2/'+this.params['query'])
+    .then((response: { data: any; }) => {
+      this.tabJeu.push(response.data);
+      console.log(this.tabJeu[1]);
+      return this.coord;
+    }).catch((error: any) => {
+      console.log(error)
+    });
+  }
   }
